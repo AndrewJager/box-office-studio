46a839f47c8e8c52c50bb404ffad593fcc68d4de
Drew J <35353143+CatsAreEvil@users.noreply.github.com>
2019-03-03 12:58:28 -0500

    scroll through schedule

--- app.py
+++ app.py
@@ -60,11 +60,23 @@ def schedule():
     movies = {}
     localSystem = BoxOffice.query.first()
     i = 0
-    while i < 8:
+    while i < 4:
         movies[i] = Movie.query.filter_by(release_date=(localSystem.currentDate + datetime.timedelta(days=i*7))).all()
         i = i + 1
         
-    return render_template("schedule.html", system=localSystem, movies=movies, datetime=datetime)
+    return render_template("schedule.html", system=localSystem, movies=movies, datetime=datetime, offset=0)
+
+@app.route('/schedule/<int:offset>')
+@app.route('/schedule/-<int:offset>')
+def schedules(offset):
+    movies = {}
+    localSystem = BoxOffice.query.first()
+    i = 0
+    while i < 4:
+        movies[i] = Movie.query.filter_by(release_date=((localSystem.currentDate + datetime.timedelta(days=offset)) + datetime.timedelta(days=i*7))).all()
+        i = i + 1
+        
+    return render_template("schedule.html", system=localSystem, movies=movies, datetime=datetime, offset=offset)
 
 @app.route('/movie/<string:id>', methods=['GET', 'POST'])
 def movie(id):
